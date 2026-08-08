089f2283000bc62cd6ad2fb4e6f28301932eb024
maksimkrichevtsov <maksim.krichevtsof@gmail.com>
2026-08-08 23:44:47 +0300

    1 Task

--- JS-TS/solutions/types.ts
+++ JS-TS/solutions/types.ts
@@ -1,9 +1,15 @@
 interface Todo {
-
+    id: number,
+    title: string,
+    description?: string;
+    status: TodoStatus;
+    readonly createdAt: Date;
 }
 
 enum TodoStatus {
-
+    PENDING = 'PENDING',
+    IN_PROGRESS = 'IN_PROGRESS',
+    COMPLETED = 'COMPLETED',
 }
 
 export { Todo, TodoStatus };
